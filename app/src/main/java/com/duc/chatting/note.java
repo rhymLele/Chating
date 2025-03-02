1b723b3dac62b19736c22b3cf0efb0dbe9c43412
rhymLele <sympathic.elf181203@gmail.com>
2025-03-02 22:06:14 +0700

    add cruicial note
    w

---
+++ app/src/main/java/com/duc/chatting/note.java
@@ -0,0 +1,9 @@
+package com.duc.chatting;
+
+public class note {
+//    - add list friend
+//    - create report,block user
+//    - add chat gpt
+//    - admin panel.
+//    - add notification chat
+}

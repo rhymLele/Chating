7767b2d3a87e90bfb51822b2f3d430fa8b967137
rhymLele <sympathic.elf181203@gmail.com>
2025-04-09 22:17:55 +0700

    add file gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,9 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.karumi:dexter:6.2.3")
+
+    implementation ("com.mesibo.api:webrtc:1.0.5")
+//    implementation ("com.guolindev.permissionx:permissionx:1.6.1")
     implementation(libs.socket.io.client)
     implementation(libs.appcompat)
     implementation(libs.material)
